b3f250f64d432951cc8c2c8a58564ae6ac79a28d
Sara Jane Mildenstein <saraj@pop-os.localdomain>
2023-10-02 15:03:30 -0600

    updated from script on 10-02-23

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ DATENOW=$(date +%m-%d-%y)
 read -p "Do you want to push to git?" yn
 case $yn in
   y ) git add .; git commit -m "updated from script on $DATENOW"; git push;;
-  n ) echo "FINE BYE <3"; exit;;
+  n ) echo "FINE BYE ❤"; exit;;
   * ) echo "invalid input also bye"; exit;;
 esac
 #done 
