1dc362ad5a0762199317705984d2e91ea49148a3
Sara Jane Mildenstein <saraj@pop-os.localdomain>
2023-10-02 13:27:51 -0600

    updated from script on %DATENOW

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+DATENOW=$(date +%m-%d-%y)
+while true; do
+read -p "Do you want to push to git?" yn
+case $yn in
+  y ) git add .; git commit -m "updated from script on %DATENOW"; git push;;
+  n ) echo "FINE BYE <3"; exit;;
+  * ) echo "invalid input also bye"; exit;;
+esac
+done 
